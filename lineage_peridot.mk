a01b0ec1419b7919491e7ab2d07d506a300f4aba
Zolg <zolg@zolg.ru>
2025-07-21 18:25:38 +0300

    dependencies fix

---
+++ lineage_peridot.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common LINEAGE stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from peridot device
+$(call inherit-product, device/xiaomi/peridot/device.mk)
+
+## Inherit from the MiuiCamera setup
+#$(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
+
+
+PRODUCT_NAME := lineage_peridot
+PRODUCT_DEVICE := peridot
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 24069PC21G
+
+#craddons
+PRODUCT_NO_CAMERA := true
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="peridot_global-user 14 UKQ1.240624.001 OS2.0.104.0.VNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240624.001/OS2.0.104.0.VNPMIXM:user/release-keys \
+    DeviceName=peridot
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
+
+
